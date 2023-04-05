ad01d71b69ba9113e4991600010986dd1be2d0ee
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-04-05 11:27:01 -0300

    2 files modified, 1 added, 1 deleted

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import './Grid.css';
+import { Body } from '../Body/index';
+import writing from "./writing.svg"
+//import writing from '../Assets/writing.svg';
+//import trash from '.../../Assets/trash.png';
+
 
 const Grid = () => {
+  //console.log(writing);
+ // console.log(trash);
+
   return (
+    
+    
     <div className="container_table">
+      <Body />
       <table className="table_format">
         <thead>
           <tr className="tr_format">
-            <th >#</th>
-            <th >Name</th>
-            <th >Email</th>
-            <th >Phone</th>
-            <th ></th>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th></th>
           </tr>
         </thead>
         <tbody className="table_bodyformat">
           <tr>
-            <th >1</th>
+            <th>1</th>
             <td>MMMM</td>
             <td>m@test.com</td>
             <td>1111111111</td>
