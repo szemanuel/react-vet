9898d62cd97cd0e1b6f9e157654570eb8acb92a4
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-04-14 16:25:13 -0300

    1 file modified, 2 added

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,14 @@
+import "./modal.css"
+
+const Modal = () => {
+  return (
+    <div className="modal-container">
+      <div className="modal-body">
+        <button className="modal-close">X </button>
+        <p> Lorem </p>
+      </div>
+    </div> 
+  );
+};
+
+export default Modal
