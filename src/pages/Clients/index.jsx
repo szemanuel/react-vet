adca96b53680f43e8cdd131e7609f56cc636b28a
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-04-14 15:51:40 -0300

    2 files modified

--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Clients.css';
 import { Grid } from '../../components/Grid/index';
 
-
 const Clients = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="loginPage">
       <header className="menu">
@@ -12,16 +12,47 @@ const Clients = () => {
         <h1 className="hamburguer">hamburguer</h1>
       </header>
       <main className="grid">
-        <input placeholder="Name"/>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses glasses" viewBox="0 0 16 16">
-  <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-</svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus-fill plus" viewBox="0 0 16 16">
-  <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z"/>
-</svg>
-      <Grid />
+        <input placeholder="Name" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-eyeglasses glasses"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
+        </svg>
+        <button
+          className="btnPlus"
+          type="button"
+          onClick={() => setShowModal(!setShowModal)}
+          value="+"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-file-earmark-plus-fill plus"
+          viewBox="0 0 16 16"
+        >
+          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z" />
+        </svg>
+        
+        <Grid />
+
+        <div>
+          {showModal && (
+            <div className="modal">
+              <div className="modalContent">
+                <div> COSO FLOTANTE </div>
+              </div>
+            </div>
+          )}
+        </div>
       </main>
-      </div>
+    </div>
   );
 };
 
