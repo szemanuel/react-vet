ed9383f94dc4cc05914c83373eb66e220abc2781
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-04-18 10:34:49 -0300

    1 file modified

--- src/pages/Clients/index.jsx
+++ src/pages/Clients/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Clients.css';
 import { Grid } from '../../components/Grid/index';
 import { Modal } from '../../components/Modal/index';
+import { NewClients } from "../NewClients/index"
 
 const Clients = () => {
   const [showModal, setShowModal] = useState(false);
@@ -42,7 +43,7 @@ const Clients = () => {
         >
           <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z" />
         </svg>
-        {showModal && <Modal />}
+        {showModal && <NewClients />}
         </button>
         <Grid />
               
