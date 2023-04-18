3322d77edb1a2a42378a5c2cbf81b8a1d44e07de
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-04-18 09:43:57 -0300

    2 files modified

--- src/pages/NewClients/index.jsx
+++ src/pages/NewClients/index.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import './NewClients.css';
 
-
 const NewClients = () => {
-
   return (
-   <div>
-     <header className="menu">
+    <div className="page">
+      <header className="">
         <h1 className="logo"> Vte </h1>
         <h1 className="welcome"> New Client</h1>
         <h1 className="hamburguer">hamburguer</h1>
       </header>
-
+      <main className="userLogin">
+        <div className="loginInputs">
+          <input className="inputType" placeholder="Full Name" />
+          <input className="inputType" placeholder="Email" />
+          <input className="inputType" placeholder="Adress" />
+          <input className="inputType" placeholder="Phone Number" />
+        </div>
+        <div className="userOptions">
+          <a href="#">Confirm</a>
+        </div>
+      </main>
     </div>
   );
 };
